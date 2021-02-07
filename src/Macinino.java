ea9e22677785fba5137df1165ce77061e1652b0d
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-02-07 18:26:31 +0100

    AbilityBot setup

---
+++ src/Macinino.java
@@ -0,0 +1,17 @@
+import org.telegram.abilitybots.api.bot.AbilityBot;
+
+public class Macinino extends AbilityBot {
+
+    public static String USERNAME = "MacininoBot";
+    protected Macinino(String botToken, int creatorId) {
+        super(botToken, USERNAME);
+        cId = creatorId;
+    }
+
+    @Override
+    public int creatorId() {
+        return cId;
+    }
+
+    protected int cId;
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    
+    public static void main(String[] args) { // args = [botToken, creatorId]
+        try {
+            TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+            api.registerBot(new Macinino(args[0], Integer.parseInt(args[1])));
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Main: needs a bot token and a creator ID"
+                               + " (both passed as arguments)");
+        } catch (NumberFormatException e) {
+            System.out.println("Main: the creator ID should be an integer");
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+
 }
