fe80ea237bee1f9fc8709daf8e095eb1cb605412
Stefano Volpe <stefano.volpe2@studio.unibo.it>
2021-02-10 01:45:34 +0100

    /iam (part 2)

--- src/bot/Macinino.java
+++ src/bot/Macinino.java
@@ -1,10 +1,13 @@
 package bot;
 
 import bot.data.Profile;
+import bot.data.Stat;
+
 import java.util.function.Consumer;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
 import org.telegram.abilitybots.api.objects.Locality;
@@ -35,9 +38,14 @@ public class Macinino extends AbilityBot {
     }
 
     private int cId;
-    final private static String TAGARGUMENTREGEX = "([+-]?)#(0-9A-Za-z+).*";
-    final private static Pattern TAGARGUMENTPATTERN =
-        Pattern.compile(TAGARGUMENTREGEX);
+    final private static String
+        TAGARGUMENTREGEX = "([+-]?)#(0-9A-Za-z+).*",
+        STATARGUMENTREGEX = "(\\d+)(.+)",
+        RANGEARGUMENTREGEX = "(\\d*),?(\\d*)(.+)";
+    final private static Pattern
+        TAGARGUMENTPATTERN = Pattern.compile(TAGARGUMENTREGEX),
+        STATARGUMENTPATTERN = Pattern.compile(STATARGUMENTREGEX),
+        RANGEARGUMENTPATTERN = Pattern.compile(RANGEARGUMENTREGEX);
 
     private Profile getProfile(MessageContext ctx) {
         final Map<Integer, Profile> profiles =
@@ -49,7 +57,7 @@ public class Macinino extends AbilityBot {
             return profiles.get(id);
     }
 
-    private void update(Profile profile, String argument) {
+    private boolean update(Profile profile, String argument) {
         Matcher matcher = TAGARGUMENTPATTERN.matcher(argument);
         if (matcher.matches()) {
             String tag = matcher.group(2);
@@ -58,16 +66,27 @@ public class Macinino extends AbilityBot {
             else
                 profile.addTag(tag);
         } else {
-            // TODO range filters
+            matcher = STATARGUMENTPATTERN.matcher(argument);
+            if (matcher.matches()) {
+                final String uom = matcher.group(2);
+                for (Stat stat : Stat.values())
+                    if (stat.uom().equals(uom)) {
+                        final int value = Integer.parseInt(matcher.group(1));
+                        if (stat.validate(value))
+                            profile.putStat(stat, value);
+                        break;
+                    }
+            } else
+                return false;
         }
-            
-            
+        return true;
     }
 
     private Consumer<MessageContext> iamAction = ctx -> {
         final Profile profile = getProfile(ctx);
         for (String argument : ctx.arguments())     
-            update(profile, argument);
+            if (!update(profile, argument))
+                silent.send(argument + " ❓", ctx.chatId());
     };
 
 }
