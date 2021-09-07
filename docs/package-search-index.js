2c9c6c599b73bf42368344271714477fffcc9bd4
FoxySeta <stefano.volpe2@studio.unibo.it>
2021-09-07 13:51:07 +0000

    Deploying to docs from @ FoxySeta/telegrindr@00518d7fd1de9be5d1bbef463c2ca47dddb6d60c 🚀

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"io.github.foxyseta.telegrindr"},{"l":"io.github.foxyseta.telegrindr.bot"},{"l":"io.github.foxyseta.telegrindr.bot.data"}]
